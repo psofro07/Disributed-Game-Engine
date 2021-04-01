8fd1830bf863c4c0f6fd182d9381ffc36534b6da
psofro07 <hardmaiden123@gmail.com>
2021-04-02 01:39:44 +0300

    Inserted Navbar in Home route and posted data from home

--- app.js
+++ app.js
@@ -3,16 +3,31 @@
 
 const express = require('express');
 const ejs = require("ejs");
+const bodyParser = require('body-parser');
 
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+app.use(bodyParser.json());                            // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));    // to support URL-encoded bodies
+
 const port = 3000;
+
+
 
 app.get('/', (req, res) => {
   res.render('index', {});
+});
+
+app.post('/home', (req, res) => {
+
+  let email = req.body.userEmail;
+  let psw = req.body.userPass;
+
+  res.render('home', { email: email, psw: psw});
+  
 });
 
 app.listen(port, () => {
