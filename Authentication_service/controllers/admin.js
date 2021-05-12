f9483082a3c503d4c79e1f57cefcdd1105b1b6e8
DjAzulu <infamousgoose48@gmail.com>
2021-05-12 12:11:18 +0300

    added update and delete user for admin

--- Authentication_service/controllers/admin.js
+++ Authentication_service/controllers/admin.js
@@ -22,8 +22,64 @@ exports.getUser = async (req, res, next) => {
               err.statusCode = 500;
             }
             next(err);
+        });  
+}
+
+
+exports.deleteUser = (req, res, next) => {
+
+    const username = req.params.username
+
+    User.findOne({where: { username: username } })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this username could not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            
+            return user.username;
+        })
+        .then(async (username) => {
+            await User.destroy({where: {username: username}});
+
+            res.status(200).json({ message: "User deleted successfully"});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+              err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+
+exports.updateUser = (req, res, next) => {
+
+    const username = req.params.username
+    const email = req.body.email;
+    const role = req.body.role;
+
+    User.findOne({where: { username: username } })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this username could not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            
+            return user.username;
+        })
+        .then(async (username) => {
+            await User.update({email: email, role: role} , {where: {username: username}});
+            
+            res.status(200).json({ message: "User updated successfully"});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+              err.statusCode = 500;
+            }
+            next(err);
         });
 
-    
-    
 }
