cde06153505d2a3db08eabc84b9194131b663399
Pavlos <hardmaiden123@gmail.com>
2021-05-12 14:28:40 +0300

    Admin table - delete

--- User_interface_service/routes/admin.js
+++ User_interface_service/routes/admin.js
@@ -5,6 +5,8 @@ const adminController = require('../controllers/admin');
 const isValidated = require('../middleware/isValidated');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get('/admin', isValidated && isLoggedIn, adminController.getUsers);
+router.get('/admin/createTable', isValidated && isLoggedIn, adminController.createTable);
+router.post('/admin/editTable', isValidated && isLoggedIn, adminController.editTable);
+router.get('/admin', isValidated && isLoggedIn, adminController.getAdmin)
 
 module.exports = router;
